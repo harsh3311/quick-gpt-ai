d3543b0d49bd502462f95ac7a730aca6f0f2ff47
harsh3311 <harshbhardwajofficial1414@gmail.com>
2024-07-03 10:56:20 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
@@ -10,5 +9,13 @@ export default defineConfig(({ mode }) => {
       'process.env.OPENAI_API_KEY': JSON.stringify(env.OPENAI_API_KEY)
     },
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: '/index.html',
+      },
+    },
+    server: {
+      historyApiFallback: true, // to handle client-side routing
+    },
   }
 })
